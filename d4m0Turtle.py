cee587378868d2f876f2eab25846859cc7e9ca2b
Damon Getsman <damo.gets@gmail.com>
2018-10-16 17:55:39 -0500

    Setting up eclipse+pydev, starting to customize source.

--- d4m0Turtle.py
+++ d4m0Turtle.py
@@ -6,29 +6,24 @@ import hlt
 
 # This library contains constant values.
 from hlt import constants
-
-# This library contains direction metadata to better interface with the game.
 from hlt.positionals import Direction
-
-# This library allows you to generate random numbers.
-import random
-
-# Logging allows you to save messages for yourself. This is required because the regular STDOUT
-#   (print statements) are reserved for the engine-bot communication.
+#import random
 import logging
+
+#d4m0 imports
+import d4m0_routines
 
 """ <<<Game Begin>>> """
 
-# This game object contains the initial game state.
 game = hlt.Game()
 # At this point "game" variable is populated with initial map data.
 # This is a good place to do computationally expensive start-up pre-processing.
 # As soon as you call "ready" function below, the 2 second per turn timer will start.
-game.ready("MyPythonBot")
+game.ready("D4m0Turtle")
 
 # Now that your bot is initialized, save a message to yourself in the log file with some important information.
 #   Here, you log here your id, which you can always fetch from the game object by using my_id.
-logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
+logging.info("Successfully hatched! My Player ID is {}.".format(game.my_id))
 
 """ <<<Game Loop>>> """
 
@@ -36,12 +31,11 @@ while True:
     # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
     #   running update_frame().
     game.update_frame()
-    # You extract player metadata and the updated map metadata here for convenience.
+    
+    #keeps things speedier
     me = game.me
     game_map = game.game_map
 
-    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
-    #   end of the turn.
     command_queue = []
 
     for ship in me.get_ships():
