f26be3a16acd1858db5b18e6002722621f9acbc8
Damon Getsman <damo.gets@gmail.com>
2018-10-17 04:03:17 -0500

    Almost got to the real work involving analytics & seeking/navigation to ore.

---
+++ d4m0_routines/seek-n-nav.py
@@ -0,0 +1,7 @@
+"""
+routines for general seeking out of halite ore resources w/basic resource
+location determination and navigation to it
+"""
+
+def def_target_halite_simple(current_turtle, halite_ranked_by_distance, halite_ranked_by_qty):
+
