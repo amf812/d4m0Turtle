f9505091da297bb0d1f5a19c1ee9fe1a41ce1e68
Damon Getsman <damo.gets@gmail.com>
2018-10-20 04:24:39 -0500

    Really a broken commit; just adding it here to be able to clone from another machine.  This version has myglobals and a bit more code in d4m0_routines, that's it.

--- d4m0_routines/analytics.py
+++ d4m0_routines/analytics.py
@@ -3,33 +3,51 @@ analytics.py
 
 """
 
-from hlt import constants, game_map
+from hlt import constants, game_map, positionals
+from . import myglobals
+
+import math
 
 #constant schitt
 Worth_Mining_Halite = constants.MAX_HALITE - (constants.MAX_HALITE * 0.2)
 Maximal_Consideration_Distance = game_map.width / 2
 
+class Halite_Position:
+    def __init__(self, pos, ore_qty):
+        self.pos = pos
+        self.ore_qty = ore_qty
+
+
 def locate_significant_halite(current_ship, current_map):
     """
     if there isn't maximal halite on this very spot, this routine will
-    locate at least 3 of the closest halite resources, with at least one being
-    a maximal halite deposit (more than 3, until maximal deposit is located,
-    perhaps up to a certain limit
+    locate 3 of the closest halite resources, with at least one being
+    a maximal halite deposit
     """
 
     #main routine has already determined whether or not we're too close to full
     #for this to be viable
 
     relative_halite = { }
-    positions = [ ]
+    positions = { }
 
     if current_map[current_ship.position].halite_amount >= Worth_Mining_Halite:
         relative_halite = { 'position' = [ current_ship.position ] }
         return { 'relative_halite' : relative_halite }
     else:
         #start snooping around
-        for outward_step in [1..Maximal_Consideration_Distance]:
-            if current_map[current_ship.position
+        start_x = math.modf(-Maximal_Consideration_Distance / 2)[1]
+        start_y = start_x
+        end_x = -startx
+        end_y = -startx
+        cur_pos = Position(current_ship.position)
+ 
+        for x in [start_x..end_x]:
+            if x == start_x:
+                #test the entire line
+                for y in [start_y..end_y]:
+                    
+        
 
 
 def locate_nearest_base(current_ship, current_map):
---
+++ d4m0_routines/myglobals.py
@@ -0,0 +1,17 @@
+from hlt import constants
+
+import logging
+
+"""
+debugging and other constant values
+"""
+
+DEBUGGING = {
+    'seek': True,
+    'locate_ore': True,
+}
+
+#constant schitt
+Worth_Mining_Halite = constants.MAX_HALITE - (constants.MAX_HALITE * 0.2)
+Maximal_Consideration_Distance = game_map.width / 2
+
