44d56b91250bcfd6eaa4c927ebbe53cf8420415d
unknown <fountaia@mail.gvsu.edu>
2019-01-10 08:49:28 -0500

    Got rid of crap and reformatted.  Also added code to find the nearest dropoff

--- d4m0Turtle.py
+++ d4m0Turtle.py
@@ -1,153 +1,88 @@
 #!/usr/bin/env python3
 # Python 3.6
 
-"""
-d4m0Turtle.py
-
-Started on: 19oct18 (or well that's when I remembered at add this note,
-anyway)
-
-Just my first attempt at getting a bot going decently in the Halite III AI
-coding competition; still have 2.75 months left to go on it right now.  So
-I guess the estimated final version has to be in right around the middle of
-January.  Good project to keep me busy in winter, and to get my skills up to
-the best level they can be (at least in py3).
-"""
-
 # Import the Halite SDK, which will let you interact with the game.
 import hlt
 
-import logging
+# This library contains constant values.
+from hlt import constants
 
-# d4m0 imports
-from d4m0_routines import analytics, seek_n_nav, myglobals, primary
+# This library contains direction metadata to better interface with the game.
+from hlt.positionals import Direction
+
+# This library allows you to generate random numbers.
+import random
+
+# Logging allows you to save messages for yourself. This is required because the regular STDOUT
+#   (print statements) are reserved for the engine-bot communication.
+import logging
 
 """ <<<Game Begin>>> """
 
+# This game object contains the initial game state.
 game = hlt.Game()
 # At this point "game" variable is populated with initial map data.
 # This is a good place to do computationally expensive start-up pre-processing.
 # As soon as you call "ready" function below, the 2 second per turn timer will start.
-game.ready("D4m0Turtle")
+game.ready("MyPythonBot")
+ship_status = {}
 
 # Now that your bot is initialized, save a message to yourself in the log file with some important information.
 #   Here, you log here your id, which you can always fetch from the game object by using my_id.
-myglobals.Misc.loggit('any', 'info', "Successfully hatched! My Player ID is {}.".format(game.my_id))
+logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
 
-# d4m0's init stuff
-turn = 0
+""" Finds the optimal dropoff for a ship """
+def findDrop( position, array ):
+    x = 1000
+    for val in array:
+        y = game_map.calculate_distance(position, array)
+        if y < x:
+            x = y
+    return x
 
 """ <<<Game Loop>>> """
 
 while True:
-    myglobals.Misc.loggit('core', 'info', " - updating frame")
+    # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
+    #   running update_frame().
     game.update_frame()
-
-    myglobals.Misc.loggit('core', 'info', " - updating 'me'")
-    # keeps things speedier
+    # You extract player metadata and the updated map metadata here for convenience.
     me = game.me
-
-    myglobals.Misc.loggit('core', 'info', " - updating 'game_map'")
-    # speed, again (see if there are any places to implement anything like
-    # this with my own structs)
     game_map = game.game_map
 
-    myglobals.Misc.loggit('core', 'info', " - initializing 'command_queue'")
+    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
+    #   end of the turn.
     command_queue = []
 
-    myglobals.Misc.loggit('core', 'debug', " -* me.get_ships() dump: " + str(me.get_ships()))
-
     for ship in me.get_ships():
-        # d4m0 schitt starts
-
-        # is this ship already in transit to mine?
-        try:
-            #everything at this block level has already had mission assigned
-            if myglobals.Variables.current_assignments[ship.id] and myglobals.Const.DEBUGGING['core']:
-                logging.debug(" Determining status of ship " + str(ship.id))
-
-            myglobals.Misc.loggit('save_state', 'debug', "  - ship id: " + str(ship.id) + " has state set to " +
-                                  myglobals.Variables.current_assignments[ship.id]['mission'])
-
-            # have we been a ramblin'?
-            potential_cmd = primary.Core.has_transit_been_too_long(turn, ship, game_map, me)
-            # I believe the following 3 lines will be duplicated thrice, and
-            # can just come after the branching schitt, as all will return a
-            # valid potential_cmd to test + append if necessary
-            if potential_cmd:
-                command_queue.append(potential_cmd)
-                continue
-
-            # I did things this way because of the potential for adding moar
-            # assignment types, this may be changed for efficiency in the near
-            # future
-            # so what the fsck is it supposed to ACTUALLY DO? - not sure, so
-            # it's just getting commented out for now
-            #elif myglobals.Variables.current_assignments[ship.id]['mission'] != 'mining' \
-            #        and myglobals.Variables.current_assignments[ship.id]['mission'] != 'transit' \
-            #        and myglobals.Variables.current_assignments[ship.id]['mission'] != 'dropoff':
-
-            #    # we should be good to send it on its way
-            #    if myglobals.Const.DEBUGGING['save_state'] or myglobals.Const.DEBUGGING['core']:
-            #        logging.info("  - Ship id: " + str(ship.id) + "  ready for mission.")
-
-            #    # so yeah, now we incorporate the whether or not to mine/dropoff
-            #    # block (now in analytics) with new indent levels
-            #    cmd_n_dest = analytics.Analyze.can_we_embark_and_start_mining(ship, game_map, me)
-            #    command_queue.append(cmd_n_dest['c_queue_addition'])
-
-            #    myglobals.Misc.save_ship_state(ship.id, 'transit', turn, cmd_n_dest['destination'])
-
-            #    if myglobals.Const.DEBUGGING['save_state'] or myglobals.Const.DEBUGGING['core']:
-            #        logging.debug(" - updated former unset state to new transit: " + str(cmd_n_dest['destination']))
-
-            elif myglobals.Variables.current_assignments[ship.id]['mission'] == 'transit':
-                potential_cmd = primary.Core.transit_processing_done_or_not(ship, game_map, turn, me)
-                if potential_cmd:
-                    command_queue.append(potential_cmd)
-                    continue
-            else:   # we must be set for dropoff; check and make sure that we're done nao
-                myglobals.Misc.loggit('core', 'info', "  - in transit to: " +
-                                      str(myglobals.Variables.current_assignments[ship.id]['destination']))
-                command_queue.append(ship.move(game_map.naive_navigate(ship,
-                                                                       myglobals.Variables.
-                                                                       current_assignments[ship.id]['destination'])))
-                myglobals.Misc.save_ship_state(ship.id, 'transit', turn,
-                                               myglobals.Variables.current_assignments[ship.id]['destination'])
-
-        except KeyError:
-            logging.debug("In KeyError try/except loop")
-
-            if not ship.is_full:
-                # for testing purposes right now we'll just send out mining no matter what
-                relative_halite = analytics.Analyze.locate_significant_halite(ship, game_map)
-                target = seek_n_nav.FindApproach.target_halite_simple(ship, game_map, relative_halite)
+        logging.info("Ship {} has {} halite.".format(ship.id, ship.halite_amount))
+        
+        if ship.id not in ship_status:
+            ship_status[ship.id] = "exploring"
+            
+        if ship_status[ship.id] == "returning":
+            if ship.position == me.shipyard.position:
+                ship_status[ship.id] = "exploring"
             else:
-                target = seek_n_nav.FindApproach.locate_nearest_base(ship, game_map, me)
-
-            myglobals.Misc.save_ship_state(ship.id, 'transit', turn, target)
-            # remember to change 'transit' in the above to something different if the ship is already over the
-            # destination ore deposit, dropoff, or base
-            #command_queue.append(game_map.naive_navigate(ship, target))
-            # this one was a tuple, too ^^^^
-            command_queue.append(ship.move(game_map.naive_navigate(ship, target)))
-
-        # d4m0 schitt ends
-
+                move = game_map.naive_navigate(ship, findDrop(me.shipyard.position, me.get_dropoffs())
+                command_queue.append(ship.move(move))
+                continue
+        elif ship.halite_amount >= constants.MAX_HALITE / 4:
+            ship_status[ship.id] = "returning"
+            
         # For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
         #   Else, collect halite.
-        #if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
-        #    command_queue.append(
-        #        ship.move(
-        #            random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
+        if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
+            command_queue.append(
+                ship.move(
+                    random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
+        else:
+            command_queue.append(ship.stay_still())
 
     # If the game is in the first 200 turns and you have enough halite, spawn a ship.
     # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
-    if game.turn_number <= 200 and me.halite_amount >= 1000 and not game_map[me.shipyard].is_occupied:
+    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
         command_queue.append(me.shipyard.spawn())
-
-    # d4m0 end of turn schitt
-    turn += 1
 
     # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
